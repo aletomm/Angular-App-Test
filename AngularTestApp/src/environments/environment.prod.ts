4c1f0268edb91d8a1dfd83fd209c9b222fdf0115
Alessandro <tommasinalessandro@gmail.com>
2020-05-13 16:15:05 +0200

    Added built files to docs folder

--- AngularTestApp/src/environments/environment.prod.ts
+++ AngularTestApp/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  APIURL: 'https://randomuser.me/api/?results=15&inc=name,email,login,phone,location',
+  APIAUTHURL: 'https://randomuser.me/api/?results=15&inc=name,email,login,phone,location'  
 };
